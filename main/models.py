792485ddfafbb22eaaeab810ed0b4d00ec5ef6ba
Muhammad Rafi Sugianto <muhammad.rafi42@ui.ac.id>
2025-09-17 15:42:21 +0700

    Tutorial 3 selesai

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 import uuid
 from django.db import models
 
 class News(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
     CATEGORY_CHOICES = [
         ('transfer', 'Transfer'),
         ('update', 'Update'),
